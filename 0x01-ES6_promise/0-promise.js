a7b3b8f763325e7918fca4c0b12495a5eb92ceaf
ElvisMw <elvismwanthi@gmail.com>
2024-04-24 16:24:32 +0300

    0-promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,6 @@
 // 0-promise.js
 export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-        // Simulate an API call
         resolve('Response from API');
     });
 }
