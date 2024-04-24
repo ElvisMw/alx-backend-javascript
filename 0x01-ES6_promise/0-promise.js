00a1ad72bb70f7ccf5bf2a11555eac34f2443c48
ElvisMw <elvismwanthi@gmail.com>
2024-04-24 16:36:28 +0300

    0-promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
-/**
- * Retrieves a response from the API.
- *
- * @return {Promise} A Promise that resolves with 'Response from API'
- */
 export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
         resolve('Response from API');
