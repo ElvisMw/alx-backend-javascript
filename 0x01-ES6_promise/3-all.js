c8873437905fad576f05ff72d71fd16c345d1d14
ElvisMw <elvismwanthi@gmail.com>
2024-04-24 18:49:24 +0300

    3-all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+// Handle profile signup using multiple promises
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`); // Log successful signup
+    })
+    .catch(() => console.log('Signup system offline')); // Log error if signup fails
+}
