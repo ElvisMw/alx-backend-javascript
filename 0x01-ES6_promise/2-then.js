a63024590afab7b7d5e2c94e5aeea5413c77ad9a
ElvisMw <elvismwanthi@gmail.com>
2024-04-24 21:50:46 +0300

    2-then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,8 +2,8 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
-      console.log('Got a response from the API'); // Log success message
-      return { status: 200, body: 'success' }; // Return success object
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
     })
-    .catch(() => new Error()); // Catch any errors
+    .catch(() => new Error());
 }
