803ee08e270f3494103f008a66471fa3599ab53e
ElvisMw <elvismwanthi@gmail.com>
2024-04-27 19:46:01 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,40 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
-    },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
-=======
   env: {
     browser: false,
     es6: true,
@@ -69,5 +33,3 @@ module.exports = {
     }
   ]
 };
-
->>>>>>> d6da864f47bc21d423d9be5459e00ef0dfee5688
