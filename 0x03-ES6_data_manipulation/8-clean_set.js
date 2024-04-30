54149e6bd228524a1fca6aec6da2067d8932ac35
ElvisMw <elvismwanthi@gmail.com>
2024-04-30 23:16:56 +0300

    8 clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
-// Function cleanSet returns a string of all the set values that start with a specific string
+// Function cleanSet returns a string of all the set values
+//that start with a specific string
 const cleanSet = (set, startString) => {
   // Initialize an empty array to store the cleaned values
   const cleanedValues = [];
@@ -7,7 +8,6 @@ const cleanSet = (set, startString) => {
   for (const value of set) {
     // Check if the current value starts with the startString
     if (value.startsWith(startString)) {
-      // If it does, push the rest of the string after startString to the cleanedValues array
       cleanedValues.push(value.slice(startString.length));
     }
   }
